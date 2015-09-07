80d22ef645ae5579cbba2d6572f36ec795789591
Mawueli Kofi Adzoe <wallclockbuilder@googlemail.com>
2015-09-07 07:58:41 +0000

    Add godoc description.

---
+++ doc.go
@@ -0,0 +1,9 @@
+/* 
+Package logrus Structured, pluggable logging for Go.
+
+Logrus is a structured logger for Go, completely API compatible with the standard library logger.
+
+Read more at https://github.com/Sirupsen/logrus
+*/
+
+package logrus
